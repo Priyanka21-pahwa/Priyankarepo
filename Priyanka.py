3192202bd1c451d57bc6f3434de65ab8752cfe3a
Priyanka21-pahwa <Priyanka.Pahwa@metropolia.fi>
2024-09-09 21:17:27 +0300

    3rd

--- Priyanka.py
+++ Priyanka.py
@@ -1,8 +1,25 @@
-length = float(input("enter the length of the rectangle: "))
-width = float(input("enter the width of the rectangle: "))
-perimeter = 2 * (length + width)
-area = length * width
-print(" Perimeter of the rectangle is: ", perimeter)
-print(" Area of the rectangle is: ", area)
+LOT_TO_GRAM = 13.3
+POUND_TO_GRAM = 32
+TALENT_TO_POUND = 20
+GRAM_IN_KILOGRAM = 1000
+talents = float(input("Enter talent: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+total_lots = (talents * pounds * lots)
+total_grams = total_lots * LOT_TO_GRAM
+kilograms = int(total_grams / GRAM_IN_KILOGRAM)
+grams = total_grams % GRAM_IN_KILOGRAM
+print(f"The weight in modern units: {kilograms:} kilograms and {grams:.2f} grams.")
+n = []
+for num in range(0,9):
+    num = str(num).zfill(3)
+    print(num)
+    n.append(num)
+    n1 =[]
+    for num in range(1,6):
+        num = str(num).zfill(4)
+        print(num)
+        n1.append(num)
+
 
 
