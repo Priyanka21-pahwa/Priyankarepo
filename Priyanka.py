c26f63395ba8da4128a42ef967e2dea6ea91fa22
Priyanka21-pahwa <Priyanka.Pahwa@metropolia.fi>
2024-09-09 14:12:17 +0300

    2

--- Priyanka.py
+++ Priyanka.py
@@ -1,18 +1,8 @@
-list1 =[]
-names = input('Enter Names:')
-while names!= 'Exit':
-    list1.append(names)
-    names = input('Enter Names')
-    print(list1)
-n = input ('Do you want to remove an element from the list?: Y or N').upper()
-if n == 'Y':
-    rn = input('which name would you like to remove?')
-    if rn in list1:
-        list1.remove(rn)
-    else:
-        print('Nope! The name you entered is not in the list.')
-    else:
-    print('Have a nice day!')
-    print('The updated list is:',list1)
+name = input("Enter your name: ")
+print("Hello " + name)
+r = float(input("Enter the radius: "))
+import math
+area = math.pi * (r ** 2)
+print("Area = ", area)
 
 
