0c418ae7ba77517278392391d329a44205c1c447
Priyanka21-pahwa <Priyanka.Pahwa@metropolia.fi>
2024-09-14 01:23:21 +0300

    1

--- Assignment4.py
+++ Assignment4.py
@@ -7,3 +7,17 @@ for _ in range(num_dice):
 print(f"The sum of the numbers rolled is: {total_sum}")
 
 
+numbers = []
+while True:
+    user_input = input("Enter a number (or press Enter to finish): ")
+    if user_input == "":
+        break
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Please enter a valid number.")
+numbers.sort(reverse=True)
+top_five_numbers = numbers[:5]
+print("The five greatest numbers in descending order are:", top_five_numbers)
+
